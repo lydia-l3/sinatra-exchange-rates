254453b9a506725a87e9dfbd98ef1e36a24f1699
lydia-l3 <122927604+lydia-l3@users.noreply.github.com>
2024-04-17 03:14:35 +0000

    final version exchange rates

--- app.rb
+++ app.rb
@@ -17,11 +17,44 @@ get("/") do
   raw_data_string = raw_data.to_s
 
   # convert the string to JSON
-  parsed_data = JSON.parse(raw_data_string)
+  @parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
-  @symbols = parsed_data["symbols"]
+  @currencies = @parsed_data["currencies"]
+  @keys = @currencies.keys
 
   # render a view template where I show the symbols
   erb(:homepage)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @currencies = @parsed_data["currencies"]
+  @keys = @currencies.keys
+  
+  # some more code to parse the URL and render a view template
+  erb(:flexible)
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # some more code to parse the URL and render a view template
+  erb(:flex)
 end
